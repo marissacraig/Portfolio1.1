4ce1aea3fa961a7dc04706b8a0e669c417015e23
Marissa Craig <135668035+marissacraig@users.noreply.github.com>
2023-11-29 19:16:33 -0800

    Update index.jsx

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -39,6 +39,17 @@ function Project() {
           </a>
         </div>
       </section>
+      <section className="post-section">
+            <div className="post-header">
+                <p>Gift Genius</p>
+                <p>React</p>
+            </div>
+            <div className="post-body">
+            <a href="https://the-gift-genius-f23e07b1b55d.herokuapp.com/"><img className="tile" src=""/></a>
+            </div>
+
+
+        </section>
       {/* <section className="post-section">
             <div className="post-header">
                 <p></p>
@@ -56,18 +67,7 @@ function Project() {
                 <p></p>
             </div>
             <div className="post-body">
-            <a href=""><img className="tile" src="./src/assets/Projects/resume-radar-logo.png"/></a>
-            </div>
-
-
-        </section> */}
-      {/* <section className="post-section">
-            <div className="post-header">
-                <p></p>
-                <p></p>
-            </div>
-            <div className="post-body">
-            <a href=""><img className="tile" src="./src/assets/Projects/resume-radar-logo.png"/></a>
+            <a href=""><img className="tile" src="gift-genius.png"/></a>
             </div>
 
 
